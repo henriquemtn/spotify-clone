15170e47ad92662b1c54533b0163b0dfb3cf56cb
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-03 13:04:28 -0300

    add: validating audio file

--- backend/spotify/songs/models.py
+++ backend/spotify/songs/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+def validate_audio_file(value):
+    valid_extensions = ['mp3', 'wav', 'ogg', 'flac']  # Adicione outras extensões conforme necessário
+    ext = os.path.splitext(value.name)[1]  # Obtém a extensão do arquivo
+    if ext.lower() not in valid_extensions:
+        raise ValidationError(f'Unsupported file extension. Allowed extensions are: {", ".join(valid_extensions)}.')
+    
 # Modelo das Músicas
 class Songs(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
-    audio_file = models.FileField(upload_to='audio/')
+    audio_file = models.FileField(upload_to='audio/', validators=[validate_audio_file])
+
 
     def __str__(self):
         return self.title
