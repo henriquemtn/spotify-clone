dbb5be7a68944ca6727a47052b3558b29915d647
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-03 21:32:27 -0300

    HOTFIX: settings.py

--- backend/spotify/spotify/settings.py
+++ backend/spotify/spotify/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     # Aplicativos do projeto
     'songs',
     'artists', 
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
