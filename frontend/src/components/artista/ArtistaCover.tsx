4efdc25afd54c4987c57f650905d7acccb72a4ad
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-04 09:49:01 -0300

    add: artist dynamic banner and infos

--- frontend/src/components/artista/ArtistaCover.tsx
+++ frontend/src/components/artista/ArtistaCover.tsx
@@ -1,30 +1,49 @@
-import { useEffect } from "react";
-import useApi from "../../hooks/useApi";
+import { useEffect, useState } from "react";
 import "./index.css";
 import VerifiedIcon from "../../assets/verified-icon.png";
+import { Artist } from "../../types/types";
+import { api } from "../../lib/axios";
 
 export default function ArtistaCover({ artistId }: { artistId: number }) {
-  const { artist, fetchArtistById, loading, error } = useApi();
+  const [artist, setArtist] = useState<Artist | null>();
 
-  // Fetch the artist by ID when the component mounts
+  // Garantir que seja uma função assincrona
   useEffect(() => {
-    fetchArtistById(artistId);
-  }, [artistId, fetchArtistById]);
+    const fetchArtist = async () => {
+      try {
+        const response = await api.get<Artist>(`/api/artists/${artistId}/`);
+        setArtist(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  // Handle loading and error states
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+    fetchArtist();
+  }, [artistId]);
+
   if (!artist) return <p>No artist data available.</p>;
 
   return (
     <div className="container-artists">
-      <img src={artist.banner} alt="Artist Banner" className="artist-banner" />
-      {artist.isVerified && (
-        <div className="verified-artist">
-          <img className="verified-icon" src={VerifiedIcon} alt="Verified Icon" />
-          <p>Artista verificado</p>
+      <div
+        className="artist-banner"
+        style={{ backgroundImage: `url(${artist.banner})` }}
+      >
+        <div className="artist-info">
+          {artist.isVerified && (
+            <div className="verified-artist">
+              <img
+                className="verified-icon"
+                src={VerifiedIcon}
+                alt="Verified Icon"
+              />
+              <p>Artista verificado</p>
+            </div>
+          )}
+          <h2 className="artist-name">{artist.name}</h2>
+          <p className="artist-listeners">16.367.365 ouvintes mensais</p>
         </div>
-      )}
+      </div>
     </div>
   );
 }
