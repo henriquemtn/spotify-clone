1ec94202fc8a1ce6d44ce7ece1d3324ae9e5f9dd
henriquemtn <henriquemartinsilveira2@gmail.com>
2024-09-04 12:28:14 -0300

    add: bottomPlayer.tsx without player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css';
-import ArtistaCover from './components/artista/ArtistaCover';
+import "./App.css";
+import ArtistaCover from "./components/artista/ArtistaCover";
+import BottomPlayer from "./components/bottom-player/BottomPlayer";
 
 function App() {
   return (
-    <ArtistaCover artistId={1}/>
+    <>
+      <ArtistaCover artistId={1} />
+      <BottomPlayer />
+    </>
   );
 }
 
---
+++ frontend/src/components/bottom-player/BottomPlayer.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const playList = [
+  {
+    name: "Ditto",
+    writer: "New Jeans",
+    img: "http://localhost:8000/media/album_covers/images.jpg",
+    src: "http://localhost:8000/media/audio/Ditto_NJkLvcl.mp3",
+    id: 1,
+  },
+];
+
+export default function BottomPlayer() {
+  return (
+      <div className="player-container">
+          teste
+    </div>
+  );
+}
